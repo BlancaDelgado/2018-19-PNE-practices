08a5aae3281aaa54d57b709c31ddf00f2b0ca29f
BlancaDelgado <blancadelgadobonet@gmail.com>
2019-01-30 17:59:49 +0100

    Working on projects

--- session-2/print_numbers.py
+++ session-2/print_numbers.py
@@ -1,5 +1,6 @@
 
-print('\nThis program prints numbers from 1-20.\n')
+print('\nThis program prints numbers from 1-20.')
+print('----------\n')
 
 for i in range(1,20+1):
     print(i)
--- session-2/sum.py
+++ session-2/sum.py
@@ -1,4 +1,7 @@
+print('-----')
+
 print('This program sums up the first 100 integer numbers.')
+print('-----\n')
 
 total_sum = 0
 for i in range(0,101): # Add until 100, range doesn't count with last value.
