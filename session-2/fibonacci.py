9d98fc185a1f8c45ec840e6ce2124b10a2b13c2c
BlancaDelgado <blancadelgadobonet@gmail.com>
2019-02-06 17:37:46 +0100

    Working on file.

--- session-2/fibonacci.py
+++ session-2/fibonacci.py
@@ -1,18 +1,37 @@
-
+print('-----')
 print('This program calculates the nth term of the fibonacci series.')
 print('-----')
 
 
 def fibonacci(nth):
-    """Calculate the nth term of the fibonacci series."""
+    """Calculates the nth term of the fibonacci series."""
 
-    total = 0  # initial value of the sum
-    for i in range(0, nth+1):  # obtain the wanted sum (fibonacci series)
-        total += i
+    if nth < 0:
+        print('ERROR: Please introduce a positive integer.\n')
+        return None
 
-    return total
+    elif nth == 0 or nth == 1:
+        sol1 = nth
+        return sol1
+
+    elif nth > 1:
+
+        first = 0
+        second = 1
+        for i in range(0, nth-1):
+            sol2 = first + second
+
+            first = second
+            second = sol2
+
+        return sol2
+
+    else:
+        print('ERROR: Sorry, the program could not find an answer for your term.')
+        return None
 
 
 # -- Main program
+
 nth = int(input('Introduce nth term: '))
 print('Value =', fibonacci(nth))
