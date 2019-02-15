feafdf3ff071caa7bdcf880f60f340cd4cfd11df
BlancaDelgado <blancadelgadobonet@gmail.com>
2019-02-15 11:53:50 +0100

    Finished: practice 1

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -17,9 +17,33 @@ s4 = Seq_1.reverse()  # reverse of third sequence
 sequences = (s1, s2, s3, s4)  # tuple with all seqs
 
 # GIVE INFORMATION OF SEQUENCES
+num = 0
 for sequence in sequences:
-    print(sequence)
+    num += 1
+    Seq_n = Seq(sequence)
+    print()
+    print('--------------------- SEQUENCE', num, '---------------------')
 
+    # Print sequence string
+    print('Sequence {}'.format(num) + ': {}'.format(sequence))
 
+    # Print total length
+    print('\tLength: {}'.format(Seq_n.len()))
 
+    # Calculate bases count and percentage
+    counting_b = []
+    counting_perc = []
+    for b in 'ATCG':
+        counting_b.append(b + ':')
+        counting_b.append('{}'.format(Seq_n.count(b)) + ' ')
 
+        counting_perc.append(b + ':')
+        counting_perc.append('{}%'.format(Seq_n.perc(b)) + ' ')
+
+    # Print number of bases of every type
+    counter_b = ''.join(counting_b)
+    print('\tBases count: ' + counter_b)
+
+    # Print percentage of bases of every type
+    counter_perc = ''.join(counting_perc)
+    print('\tBases percentage: ' + counter_perc)
