418e36888087b7c0132cba6121bf1978015aa3ee
BlancaDelgado <blancadelgadobonet@gmail.com>
2019-03-28 16:51:05 +0100

    Working: practice 6 (check operations)

--- P6/webserver-seq.py
+++ P6/webserver-seq.py
@@ -1,110 +1,139 @@
 # Web server for performing operations on a sequence
 
-# PREDESIGNED OPERATIONS ON SEQUENCES
-from P1.Seq import Seq
-
-# LIBRARIES FOR WEB SERVERS
 import http.server
 import socketserver
 import termcolor
 
-PORT = 8014
+from P1.Seq import Seq
+
+PORT = 8002
+
+def attend_seq(seq):
+    """
+    Checks sequence submitted to see if it is valid or not.
+    If it is valid, it capitalizes it.
+    :param seq: sequence submitted in form
+    :return: False ---> if not valid
+    :return: seq------> if valid
+    """
+
+    # CHECK FOR EVERY CHARACTER IN POSSIBLE ACGT
+    for i in seq.upper():
+        if i not in 'ACGT':
+            return False
+
+    # IF ALL OF THEM WORK OUT
+    seq = seq.upper()
+    return seq
 
 
-class TestHandler(http.server.BaseHTTPRequestHandler):
+class SeqHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
 
-        def response(client_msg):
-            # MAKE A LIST WITH THE ELEMENTS OF THE MESSAGE
-            client_msg = client_msg.replace('GET /myserver?', '').replace(' HTTP/1.1', '').split("&")
-
-            if len(client_msg) == 4 or len(client_msg) == 3:
-
-                # MAKE A DOUBLE LIST OF THE MESSAGE (TYPE/COMMAND)
-                queries = []
-                for i in client_msg:
-                    query = i.split('=')
-                    command_type = query[0]
-                    command = query[1]
-
-                    i = [command_type, command]
-                    queries.append(i)
-
-                # ATTEND ALL QUERIES
-                for pair in queries:
-
-                    if pair[0] == 'msg':
-                        seq = pair[1].upper()
-                        Seq_Class = Seq(seq)
-
-                        for i in seq:
-                            if i not in ['ACGT']:
-                                error = """
-                                        <html lang="en" dir="ltr">
-                                            <head>
-                                                <meta charset="UTF-8">
-                                                <title>Error Page<title>
-                                            </head>
-
-                                            <body>
-                                                <br><h3>ERROR PAGE:</h3>
-                                                <p>Sorry, your sequence could not be identified.</p>
-                                            </body>
-
-                                        </html>
-
-                                        """
-                                return error
-
-
-
-                        return error
-
-
-
-                print(queries)
-
-
-
-            else:
-                pass
-
-
-        # PRINT REQUEST LINE FROM CLIENT
+        # PRINT REQUEST LINE
         termcolor.cprint(self.requestline, 'green')
 
-        # SELECT FILE ACCORDING TO REQUEST
-        # request = self.requestline.split(" ")[1]
+        # READ FILE DEPENDING ON PATH
+        # If we are starting program
+        if self.path == '/' or 'favicon' in self.path:
+            f_200 = open('main.html', 'r')
+            contents = f_200.read()
 
-        filename = 'main.html'
+        # If sequence is sent
+        elif 'seq' in self.path:
 
-        # READ SELECTED FILE
-        file = open(filename, 'r')
-        contents = file.read()
+            path = self.path.replace('/seq?', '').replace(' HTTP/1.1', '')
+            inputs = path.split('&')
 
-        # PRINT RESPONSE MESSAGE ON TERMINAL
-        self.send_response(200)  # OK
+            input_seq = inputs[0].split('=')[1]
+
+            # CHECK SEQUENCE SUBMITTED
+            seq = attend_seq(input_seq)
+
+            if not seq:  # if there IS NOT sequence, error
+                f_error = open('error.html', 'r')
+                contents = f_error.read()
+
+            # ANALYZE INPUTS
+            else:  # if there IS sequence, continue with inputs
+
+                SeqClass = Seq(seq)
+
+                for i in inputs:
+                    i_type = i[0]
+                    i_value = i[1]
+
+                    # 1.- CHECK LENGTH
+                    len = ""  # in case it is not selected
+                    if i_type == 'chk':
+                        len = SeqClass.len()
+                        len = "<p>· Sequence length: {len}<p>".format(len=len)
+
+                    # 2.- CHECK OPERATIONS
+                    count = False
+                    perc = False
+                    elif i_type == 'operation':
+                        if i_value == 'count':
+                            count = True
+                        elif i_value == 'perc':
+                            perc = True
+
+                    # 3.- CHECK BASE
+                    elif i_type == 'base':
+                        base = i_value
+
+                # CALCULATE OPERATIONS
+                if count:
+                    count = SeqClass.count(base)
+                    count = "<p>·Operation count on the {base}: {count}".format(base=base, count=count)
+
+                elif perc:
+                    perc = SeqClass.perc(base)
+                    perc = "<p>·Operation percentage on the {base}: {perc}".format(base=base, perc=perc)
+
+
+                contents = """
+                <html>
+                <header>
+                    <meta charset="UTF-8">
+                    <title>Result</title>
+                </header>
+                
+                <body>
+                    <h1><u><RESULTS</u></h1>
+                    <p>· Sequence given by user: {seq}</p><br>
+                    
+                """ + len + """
+                </body>
+                
+                
+                </html>
+                """
+
+
+        # If the resource doesn't exist
+        else:
+            f_error = open('error.html', 'r')
+            contents = f_error.read()
+
+        # PRINT RESPONSE MESSAGE
+        self.send_response(200)
         self.send_header('Content-Type', 'text/html')
-        self.send_header('Content_Length', len(str.encode(contents)))
+        self.send_header('Content-Length', len(str.encode(contents)))
         self.end_headers()
 
-        # TAKE CARE OF RESPONSE
-        response(self.requestline)
-
-        contents = response(self.requestline)
-        # Send body of response message to client
+        # SEND BODY OF RESPONSE MESSAGE
         self.wfile.write(str.encode(contents))
 
 
-# -- main program
-
-with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
-    print("Serving at PORT: {}".format(PORT))
+# -- Main program
+with socketserver.TCPServer(('', PORT), SeqHandler) as httpd:
+    print('Serving at PORT: {}'.format(PORT))
 
     try:
         httpd.serve_forever()
 
     except KeyboardInterrupt:
-        print("Exit by user.")
+        print('Exit by user.')
         httpd.server_close()
