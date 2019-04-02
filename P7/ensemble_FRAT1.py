8f9e4f6b098b27c49e3cd53099ad5a330c29da2c
BlancaDelgado <blancadelgadobonet@gmail.com>
2019-04-02 16:42:56 +0200

    Working: practice 7

--- P7/ensemble_FRAT1.py
+++ P7/ensemble_FRAT1.py
@@ -1,1 +1,22 @@
-# JSON PRACTICE
+# Python client that connects to the ensemble database (using JSON!)
+
+# USEFUL DATA...
+# Ensemble: http://rest.ensembl.org/
+# Source for getting a sequence: sequence/id/
+
+
+# GET FRAT1 GENE (w/ REST API)
+
+
+
+# PERFORM CALCULATIONS
+
+# 1.- Number of bases in gene
+
+# 2.- Number of T bases in gene
+
+# 3.- Most popular base and percentage
+
+# 4.- Percentage of all bases in gene
+
+
