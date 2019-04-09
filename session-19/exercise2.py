48a67272fe16d984903975f8e8e65de9fdf65e66
BlancaDelgado <blancadelgadobonet@gmail.com>
2019-04-09 16:40:26 +0200

    Working: practice 7 (stuck)

--- session-19/exercise2.py
+++ session-19/exercise2.py
@@ -6,41 +6,72 @@ import json
 import termcolor
 
 
-def connect(ENDPOINT, woeid):
+def connect(ENDPOINT):
+    """
+    Connects to metaweather and obtains response depending on the ENDPOINT requested.
+    :param ENDPOINT: web address to request specific operations.
+    :return: json file with requested information.
+    """
 
+    # define all variables
     HOSTNAME = 'www.metaweather.com'
     METHOD = "GET"
 
-    termcolor.cprint('Connecting: http://' + HOSTNAME + ENDPOINT + woeid + '/')
+    headers = {'User-Agent': 'http-client'}
+
+    # CONNECT
+    termcolor.cprint('Loading: http://' + HOSTNAME + ENDPOINT, 'green')
     conn = http.client.HTTPSConnection(HOSTNAME)
-    conn.request = (METHOD, ENDPOINT + woeid + '/')
+    conn.request = (METHOD, ENDPOINT, None, headers)
 
     r = conn.getresponse()
-    termcolor.cprint('Response received {num} {OK}'.format(num=r.status, OK=r.reason))
-
-    txt_json = r.read().decode('utf-8')
+    txt_json = r.read().decode("utf-8")
     data = json.load(txt_json)
 
-    conn.close()
-
+    termcolor.cprint('Response received {num} {OK}'.format(num=r.status, OK=r.reason), 'green')
     return data
 
 
+def get_woeid(city):
+
+    endpoint = '/api/location/search/?query='+city
+    data = connect(endpoint)
+
+    # ELABORATED RESPONSE
+    if data:
+        print('Data:', data)
+        city_woeid = data[0]['woeid']
+
+    else:
+        city_woeid = False
+        print('ERROR: this city is not in the data base.\n')
+
+    return city_woeid
+
+
+# -- Main program
+
+n = 8
+print('-'*n + ' WEATHER ' + '-'*n)
+try:
+
+    while True:
+
+        # 1.- ASK FOR CAPITAL
+        capital = input('Introduce location: ').lower()
+
+        # Find woeid of city
+        woeid = get_woeid(capital)
+
+        # 2.- PRINT INFO
+
+        # Time
+
+        # Temperature
+
+        # Sunset
 
 
 
-
-# FIND WOEID OF CITY
-
-
-# ERROR IF NOT FOUND
-
-
-
-
-# 1.- ASK FOR CAPITAL
-
-capital = input('Introduce location: ')
-
-
-# 2.- PRINT INFO (TIME, TEMP, SUNSET)
+except KeyboardInterrupt:
+    sys.exit('EXIT BY USER.')
