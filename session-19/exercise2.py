aba86b9094e0c55e5250d6916c2bd3586b1212df
BlancaDelgado <blancadelgadobonet@gmail.com>
2019-04-10 18:16:29 +0200

    Saliendo de la mierda :)

--- session-19/exercise2.py
+++ session-19/exercise2.py
@@ -1,66 +1,29 @@
-# Analysing the weather of capitals
+# Weather of capitals
 
-import sys
+import termcolor
 import http.client
 import json
-import termcolor
 
+termcolor.cprint('Hello', 'blue')
 
-# -- Main program
+HOSTNAME = "www.metaweather.com"
 
-n = 8
-print('-'*n + ' WEATHER ' + '-'*n)
-try:
+city = input('Introduce capital:').lower()
 
+ENDPOINT = "/api/location/search/?query=" + city
 
+METHOD = "GET"
+headers = {'User-Agent': 'http-client'}
 
-    # 1.- ASK FOR CAPITAL
-    city = input('Introduce location: ')
-    city = city.lower()
+conn = http.client.HTTPSConnection(HOSTNAME)
 
-    # 2.- FIND WOEID OF CITY
+conn.request(METHOD, ENDPOINT, None, headers)
 
-    # define variables for connection
-    ENDPOINT = '/api/location/search/?query=' + city
+r = conn.getresponse()
 
-    HOSTNAME = "www.metaweather.com"
-    METHOD = "GET"
+print('Response received {num} {OK}'.format(num=r.status, OK=r.reason))
 
-    headers = {'User-Agent': 'http-client'}
+txt_json = r.read().decode("utf-8")
+conn.close()
 
-    # CONNECT
-    termcolor.cprint('Loading: http://' + HOSTNAME + ENDPOINT, 'green')
-    conn = http.client.HTTPSConnection(HOSTNAME)
-    conn.request = (METHOD, ENDPOINT, None, headers)
-
-    r = conn.getresponse()
-    txt_json = r.read().decode("utf-8")
-    conn.close()
-
-    data = json.load(txt_json)
-
-    termcolor.cprint('Response received {num} {OK}'.format(num=r.status, OK=r.reason), 'green')
-
-    # ELABORATED RESPONSE
-    if data:
-        print('Data:', data)
-        city_woeid = data[0]['woeid']
-
-    else:
-        city_woeid = False
-        print('ERROR: this city is not in the data base.\n')
-
-
-    print(city_woeid)
-    # 2.- PRINT INFO
-
-    # Time
-
-    # Temperature
-
-    # Sunset
-
-
-
-except KeyboardInterrupt:
-    sys.exit('EXIT BY USER.')
+print(txt_json)
